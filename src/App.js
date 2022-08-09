e8555ec8956a82c98963b96a400c8512556043f2
ivsp <ivsp93@gmail.com>
2022-08-10 01:48:21 +0200

    Resuelvo problemas de cors

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import usersRouter from "./users/users.router.js";
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(cors()); //Permite resolver el problema de cors
 app.use(express.json());
 app.use(express.urlencoded({ extended: "utf-8" }));
 
