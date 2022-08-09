922b0ec5d03b7e5733b939eb024282edb4791fa3
ivsp <ivsp93@gmail.com>
2022-08-10 01:52:14 +0200

    Añado configuracion de las cors

--- src/App.js
+++ src/App.js
@@ -6,7 +6,13 @@ import usersRouter from "./users/users.router.js";
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(cors()); //Permite resolver el problema de cors
+app.use(
+  cors({
+    origin: "*",
+    credentials: true, //access-control-allow-credentials:true
+    optionSuccessStatus: 200,
+  })
+); //Permite resolver el problema de cors
 app.use(express.json());
 app.use(express.urlencoded({ extended: "utf-8" }));
 
