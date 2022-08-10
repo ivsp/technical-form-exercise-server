348d7a22038e618940c72d1d0d151d6033e7c0e7
ivsp <ivsp93@gmail.com>
2022-08-10 16:47:53 +0200

    primer commit

--- src/App.js
+++ src/App.js
@@ -6,13 +6,7 @@ import usersRouter from "./users/users.router.js";
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(
-  cors({
-    "Access-Control-Allow-Origin": "http://localhost:3000/",
-    credentials: true, //access-control-allow-credentials:true
-    optionSuccessStatus: 200,
-  })
-); //Permite resolver el problema de cors
+app.use(cors()); //Permite resolver el problema de cors
 app.use(express.json());
 app.use(express.urlencoded({ extended: "utf-8" }));
 
